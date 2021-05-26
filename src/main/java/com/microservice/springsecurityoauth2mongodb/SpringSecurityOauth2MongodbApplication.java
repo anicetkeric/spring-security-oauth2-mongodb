e191b14c8cbefa77ef7378d3a69bab3ba9b8b85c
aek <konan.kouame@soprasteria.com>
2021-05-26 20:23:07 +0200

    basic architecture

---
+++ src/main/java/com/microservice/springsecurityoauth2mongodb/SpringSecurityOauth2MongodbApplication.java
@@ -0,0 +1,13 @@
+package com.microservice.springsecurityoauth2mongodb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurityOauth2MongodbApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringSecurityOauth2MongodbApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/microservice/springsecurityoauth2mongodb/SpringSecurityOauth2MongodbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.microservice.springsecurityoauth2mongodb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecurityOauth2MongodbApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
